3a416cb8fe479ee2ba28c0047f8e1978ae75bd1d
Taha Hamim Boughanmi <99847753+tahahamim86@users.noreply.github.com>
2025-04-22 13:59:36 +0100

    updated signals

--- block/signals.py
+++ block/signals.py
@@ -1,8 +1,16 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_delete
 from django.dispatch import receiver
-from .models import DiseaseDiagnosis
+from .models import DiseaseDiagnosis, DiagnosisBlock
 from .utils import rebuild_blockchain
 
+@receiver(pre_delete, sender=DiseaseDiagnosis)
+def delete_block_before_diagnosis(sender, instance, **kwargs):
+    # Delete the corresponding block before diagnosis is removed
+    try:
+        block = DiagnosisBlock.objects.get(diagnosis=instance)
+        block.delete()
+    except DiagnosisBlock.DoesNotExist:
+        pass
 
 @receiver([post_save, post_delete], sender=DiseaseDiagnosis)
 def handle_diagnosis_change(sender, instance, **kwargs):
